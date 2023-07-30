609aa2fdf2d662256390aa95266615f338ca2b19
HARI PRASANTH.S <114271109+Hariprasanthkiot@users.noreply.github.com>
2023-07-30 18:05:40 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+const list = document.getElementById('mainList')
+console.log(list)
+
+// function callme(){
+//     console.log("Clicked!!!")
+//     list.innerHTML += '<li>New Element</li>'
+// }
+
+function tamilnadu(){
+    console.log('you clicked tamilnadu')
+}
+function kerala(){
+    console.log('you clicked kerala')
+    
+}
